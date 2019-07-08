b4f0d87444a274fe86d5a2daf05e295188645cad
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-07-08 10:15:46 +0300

    Redact variable

--- time_service.rb
+++ time_service.rb
@@ -13,50 +13,58 @@ module TimeService
 
     # matching rule with string
     def rule
-      @time_match = @time.strip.match(/^(12|11|10|0?\d):([012345]\d)\s+(AM|PM)/)
+      @time.strip.match(/^(12|11|10|0?\d):([012345]\d)\s+(AM|PM)/)
     end
 
     # throw error on invalid time
     def error
-      rule
-      raise(ArgumentError, "Invalid time: #{@time.strip}") unless @time_match
+      raise(ArgumentError, "Invalid time: #{@time.strip}") unless rule
     end
 
     # transform MatchData into array
     def transform
       error
-      @str_hours, @str_minutes, @meridian = @time_match.captures
+      str_hours, str_minutes, meridian = rule.captures
+      { hours: str_hours.to_i, minutes: str_minutes.to_i, meridian: meridian }
     end
 
     # return meridian
-    def meridian(adjusted_hours)
+    def meridian_a(adjusted_hours)
       if adjusted_hours > 12
         @adjusted_hours -= 12
-        @meridian = 'PM'
+        'PM'
       else
-        @meridian = 'AM'
+        'AM'
       end
     end
 
     def add_minutes
-      transform
-      hours = (@meridian == 'AM' ? @str_hours.to_i : @str_hours.to_i + 12)
-      total_minutes = hours * 60 + @str_minutes.to_i + @min
+      a = transform
+      str_hours = a[:hours]
+      str_minutes = a[:minutes]
+      meridian = a[:meridian]
+      hours = (meridian == 'AM' ? str_hours : str_hours + 12)
+      total_minutes = hours * 60 + str_minutes + @min
       # we only want the minutes that fit within a day
-      total_minutes = total_minutes % (24 * 60)
-      @adjusted_hours, @adjusted_minutes = total_minutes.divmod(60)
-      meridian(@adjusted_hours)
+      total_minutes % (24 * 60)
+    end
+
+    def split_minutes
+      @adjusted_hours, adjusted_minutes = add_minutes.divmod(60)
+      mer = meridian_a(@adjusted_hours)
+      [@adjusted_hours, adjusted_minutes, mer]
     end
   end
 
   # subclass
   class TimeEdit < InitialTime
     def output_result
-      add_minutes
-      printf('%d:%02d %s', @adjusted_hours, @adjusted_minutes, @meridian)
+      a = split_minutes
+      '%d:%02d %s' % [a[0], a[1], a[2]]
     end
   end
 end
 
-test = TimeService::TimeEdit.new('11:59 AM', 50)
-test.output_result
+# test = TimeService::TimeEdit.new('10:00 AM', 600).add_minutes
+
+p TimeService::TimeEdit.new('10:00 AM', 600).output_result
